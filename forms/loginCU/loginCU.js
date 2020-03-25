108078e83e0b142677efa9dac3c21e3e1bff9576
valeriegamon <valeriegamon@Valeries-MacBook-Pro.local>
2020-03-25 14:28:44 -0700

    Update loginCU.js

--- forms/loginCU/loginCU.js
+++ forms/loginCU/loginCU.js
@@ -3,19 +3,21 @@
 //If they aren't authenticated, reset the input controls to blank and give them a 
 //message that they can try again as their login was not valid. 
 
+req = Ajax("https://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inptNetID.value + "&j_password=" + inptNetIDPassword.value);
+
+
 btnLogin.onclick=function(){
-  
-    req = Ajax("https://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inptNetID.value + "&j_password=" + inptNetIDPassword.value);
+
     if (req.status == 200) { //everything worked.
         //lblWelcome.style.display = "block"  // none to hide
         //lblResult.style.display = "block"  // none to hide
         //lblResult.value = "The authentication code is " + req.responseText
         //1 good 0 bad
         
-        if (req.responseText == 1) {
+        if (req.responseText == 1) 
             ChangeForm(favFoods)
-        } else {
-            NSB.MsgBox("That login is not recognised. Please try again.")
+        else {
+            NSB.MsgBox("Sorry, that login is not recognized. Please try again.")
             inptNetID.value = ""
             inptNetIDPassword.value = ""
             }
