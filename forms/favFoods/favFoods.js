3b1ddcfddd7af60163814515281f5f7003a0c28d
valeriegamon <valeriegamon@Valeries-MacBook-Pro.local>
2020-03-25 16:44:00 -0700

    3/25/2020

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -3,7 +3,6 @@ Add a button on the right bottom corner of the form for navigation,
 When clicked, goes to the next page - the dessertVoting form. 
 */
 
-
 let foods = ["pizza","apples","cookies","sushi","pasta","steak"]
 
 favFoods.onshow=function(){
@@ -20,7 +19,7 @@ lstFoods.onclick=function(choice){  // notice the 'choice' parameter. This is th
       
   let textChoice = NSB.$("lstFoods_" + choice).textContent
   //NSB.MsgBox(`You picked ${textChoice}! That is a great choice!!`)
-  lblOutputFoods(`You picked ${textChoice}! That is a great choice!!`)
+ 
   }
 }
 
@@ -33,5 +32,9 @@ lstFoods.onclick=function(choice){  // notice the 'choice' parameter. This is th
     // add a new item just for fun. It will go onto the end of the list
     // 3 arguments to addItem are: (item to add,type (active or disabled), appearance) - runtime
   lstFoods.addItem("Garbanzo","active","default")
+
+lblOutputFoods.onclick=function(){
+   lblOutputFoods = (`You picked ${textChoice}! That is a great choice!!`)
+}
 
 */
