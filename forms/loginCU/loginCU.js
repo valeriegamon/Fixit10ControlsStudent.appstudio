0cf6fa94cff11d9a85297e618f6e62fe2a98f1b6
valeriegamon <valeriegamon@Valeries-MacBook-Pro.local>
2020-03-25 14:21:43 -0700

    commit

--- forms/loginCU/loginCU.js
+++ forms/loginCU/loginCU.js
@@ -3,20 +3,23 @@
 //If they aren't authenticated, reset the input controls to blank and give them a 
 //message that they can try again as their login was not valid. 
 
-let usernameNetID = ""
-//let passwordNetID = ""
-
 btnLogin.onclick=function(){
-  usernameNetID = inptNetID.value
-  let passwordNetID = inptNetIDPassword.value
-  if ((usernameNetID == "Smith") && (passwordNetID == "123Dogs#")) 
-    ChangeForm(favFoods)
-  else    
-    NSB.MsgBox("That login is not recognised. Please try again.")
-    inptNetID.value = ""
-    inptNetIDPassword.value = ""
+  
+    req = Ajax("https://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inptNetID.value + "&j_password=" + inptNetIDPassword.value);
+    if (req.status == 200) { //everything worked.
+        //lblWelcome.style.display = "block"  // none to hide
+        //lblResult.style.display = "block"  // none to hide
+        //lblResult.value = "The authentication code is " + req.responseText
+        //1 good 0 bad
+        
+        if (req.responseText == 1) {
+            ChangeForm(favFoods)
+        } else {
+            NSB.MsgBox("That login is not recognised. Please try again.")
+            inptNetID.value = ""
+            inptNetIDPassword.value = ""
+            }
+    } else 
+        //Handle that. 
+        NSB.MsgBox("The app could not connect to the server. Please try again.")
 }
-
-
-
-
