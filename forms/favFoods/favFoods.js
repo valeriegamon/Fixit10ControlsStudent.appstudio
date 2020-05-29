ddd72fc1a516880478b8da1393fe68f3fe359a8d
valeriegamon <valeriegamon@Valeries-MBP.lan>
2020-05-29 09:57:20 -0500

    Food Update
    Update

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -7,14 +7,15 @@ favFoods.onshow=function(){
         lstFoods.addItem(foods[i])
 }
 
-lstFoods.onclick=function(choice){  // notice the 'choice' parameter. This is the index
-  if (typeof(choice) == "object") {   // user clicked the control
+lstFoods.onclick=function(choice){  
+    if (typeof(choice) == "object") {   // user clicked the control
     return
   } else {
-      
-  let textChoice = NSB.$("lstFoods_" + choice).textContent
-  NSB.MsgBox(`You picked ${textChoice}! That is a great choice!!`)
- 
+    let textChoice = NSB.$("lstFoods_" + choice).textContent
+
+    lblOutputFoods.onclick=function() {
+      lblOutputFoods = (`You picked ${textChoice}! That is a great choice!!`)
+      }
   }
 }
 
@@ -25,22 +26,12 @@ btnDessertVoting.onclick=function(){
 /* 
 Use a light blue label for output. 
 
-Change the Listgroup properties so that: 
+??????Change the Listgroup properties so that: 
 the Listgroup is a button-type, that highlights a choice when the user clicks on it. 
 
-
-
-  // how to change text of an item already on the list
-  // change Teddy, at location 2, to Esmerelda
-  let newPlace = 0   //index where you want replacement to go
-  NSB.$("lstFoods_" + newPlace).textContent = "Esmerelda"
-  
-    // add a new item just for fun. It will go onto the end of the list
-    // 3 arguments to addItem are: (item to add,type (active or disabled), appearance) - runtime
-  lstFoods.addItem("Garbanzo","active","default")
-
 lblOutputFoods.onclick=function(){
-   lblOutputFoods = (`You picked ${textChoice}! That is a great choice!!`)
-}
+      lblOutputFoods = (`You picked ${NSB.$("lstFoods_" + choice).textContent}! That is a great choice!!`)
 
+let textChoice = NSB.$("lstFoods_" + choice).textContent
+NSB.MsgBox(`You picked ${textChoice}! That is a great choice!!`)
 */
