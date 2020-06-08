264be6cf6dc21e90df8b4c26062f1ac92b4bc2dd
valeriegamon <valeriegamon@Valeries-MBP.lan>
2020-06-08 13:55:48 -0500

    Updates

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -1,12 +1,77 @@
 var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
-favExercises.onshow=function(){
-
+function arrayUnique(array) {
+    var a = array.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+    return a;
 }
+
+var exercisesAll = arrayUnique(exercises.concat(coreExercises));
+
+favExercises.onshow=function(){
+    selExercises.clear()   
+    for (i = 0; i <= exercisesAll.length - 1; i++) 
+        selExercises.addItem(exercisesAll[i])
+}
+
+//how to select more than one item
+//how to add if/then for a couple options
+//what is the modal window (see comment below) 
+
+
+
+
+
 
 selExercises.onfocusout=function(){
 
 }
 
 
+
+
+
+
+
+
+
+
+
+
+btnMobileNav.onclick=function(){
+  ChangeForm(mobileNav)
+}
+
+btnFavExercies2.onclick=function(){
+  ChangeForm(favExercises)
+}
+
+/*
+Scenario: The user will be able to select the two exercises from a list 
+that are best for the core (in case you don't know, these would be situps 
+and planks). 
+
+You can use an NSB.MsgBox(). 
+Add a button so when the user has chosen their two exercises, it shows 
+them in an NSB.MsgBox that uses this format: 
+        You chose situps and planks - those are the two core exercises. 
+OR 
+       You did not pick the two core exercises. 
+Instead of showing the output in an NSB.MsgBox(), add a button that, 
+when they have chosen their two exercises, tells them in a modal window 
+what their choices were in the format: 
+       You chose situps and pushups. 
+Hint: be sure to look at the Modal example in the LoginCUModal 
+project in the repository.
+Add a button to the modal window that, when clicked, returns to the 
+favExercises form. 
+
+//let exerciseChoice = NSB.$("selExercises" + choice).textContent
+//NSB.MsgBox(`You picked ${exerciseChoice}! That is a great choice!!`)
+*/
--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -6,3 +6,34 @@ hmbMenu.onclick=function(s){
      they chose. 
   */
 }
+
+
+
+
+
+
+
+/*
+Scenario: the user will be able to select where they want to go in 
+the project by clicking on the name of a form from a hamburger menu. 
+
+Requirements
+Name all controls, using 3 letter acronyms for first three letters. 
+Only appStudio controls can be used in the app for interaction with 
+the user, I & O (Input and Output). No alerts, popups, prompts, etc. 
+Use template literals for all output. (except NSB.MsgBox() in which 
+it doesn't work)
+Add the form names to the hamburger menu control at design time. 
+Use these names: 
+
+Note: you may have to abbreviate some of these.  
+Login
+Favorite Foods
+Dessert Voting
+Describe Me
+Favorite Exercises (or AB Favorite Exercises)
+Complete the hmbMenu.onclick event handler by using a switch 
+statement so it takes the choice the user made and takes them to the 
+corresponding form.
+Hint: ChangeForm() will come in handy here.....  
+*/
